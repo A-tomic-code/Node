d5a00431837dbbadf2f5588c86a8adfa00e4d289
Daniel <danielvazquezheredia1994@gmail.com>
2022-08-11 17:24:13 +0200

    termino dia4

--- dia1/Person.js
+++
@@ -1,9 +0,0 @@
-class Person {
-    constructor(name, surname, age) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-    }
-}
-
-module.exports = Person;
--- dia1/reto1.js
+++
@@ -1,6 +0,0 @@
-console.log('Mensaje 1');
-
-setTimeout(() => {
-    console.log('Mensaje 2');
-    console.log('Mensaje 3');
-}, 3000);
