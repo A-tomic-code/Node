ad270f8d22188a46f3db627c627dfa64a262c2f2
Daniel <danielvazquezheredia1994@gmail.com>
2022-08-11 11:21:56 +0200

    acabo dia3

--- dia1/proyecto_node/index.js
+++
@@ -1,6 +0,0 @@
-const writeAndRead = require('./writeAndReadObject.js')
-const readConsole = require('./readConsole.js');
-
-readConsole((person) => {
-    writeAndRead('./reto4.json', person);
-});
